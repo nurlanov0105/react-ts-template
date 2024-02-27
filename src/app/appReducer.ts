90a7574237b91b3ce59657672537bfb686f68c70
Nursultan <nurlanov0105@gmail.com>
2024-02-27 17:42:05 +0600

    change

---
+++ src/app/appReducer.ts
@@ -0,0 +1,18 @@
+// import { discoverSlice, toursApi } from '@/entities/discoverCard';
+// import { recommendsApi, recommendsSlice } from '@/entities/recommendCard';
+// import { authApi, userSlice } from '@/features/auth';
+// import { bookingApi, bookingsSlice } from '@/features/bookForm';
+// import { detailsApi } from '@/features/placeInfo';
+// import { combineReducers } from '@reduxjs/toolkit';
+
+// export const rootReducer = combineReducers({
+//    user: userSlice,
+//    discover: discoverSlice,
+//    recommends: recommendsSlice,
+//    bookings: bookingsSlice,
+//    [toursApi.reducerPath]: toursApi.reducer,
+//    [recommendsApi.reducerPath]: recommendsApi.reducer,
+//    [detailsApi.reducerPath]: detailsApi.reducer,
+//    [authApi.reducerPath]: authApi.reducer,
+//    [bookingApi.reducerPath]: bookingApi.reducer,
+// });
--- src/app/appStore.ts
+++ src/app/appStore.ts
@@ -1,5 +1,27 @@
 // import { configureStore } from '@reduxjs/toolkit';
+// import { recommendsApi } from '@/entities/recommendCard';
+// import { toursApi } from '@/entities/discoverCard';
+// import { detailsApi } from '@/features/placeInfo';
+// import { authApi } from '@/features/auth';
+// import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+// import { rootReducer } from './appReducer';
+// import { bookingApi } from '@/features/bookForm';
 
 // export const store = configureStore({
-//    reducer: {},
+//    reducer: rootReducer,
+
+//    middleware: (getDefaultMiddleware) =>
+//       getDefaultMiddleware().concat(
+//          toursApi.middleware,
+//          recommendsApi.middleware,
+//          detailsApi.middleware,
+//          authApi.middleware,
+//          bookingApi.middleware
+//       ),
 // });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
+// export const useAppDispatch: () => AppDispatch = useDispatch;
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
